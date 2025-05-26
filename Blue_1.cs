ad2a5ed2fd2f6ffe228a0a7ed76081f6e5bfbbf5
Sasha Sivkov <58732809+sasha2412@users.noreply.github.com>
2025-05-26 18:16:49 +0300

    Add files via upload

---
+++ Blue_1.cs
@@ -0,0 +1,66 @@
+﻿using Lab8;
+using System;
+using System.Linq;
+
+namespace Lab_8
+{
+
+    public class Blue_1 : Blue
+    {
+        private string[] _output;
+
+        public string[] Output => _output;
+
+        public Blue_1(string input) : base(input)
+        {
+            _output = null;
+        }
+
+        private static void Add(ref string[] strings, string str)
+        {
+            if (strings == null || string.IsNullOrEmpty(str))
+                return;
+
+            string[] newStrings = new string[strings.Length + 1];
+            Array.Copy(strings, newStrings, strings.Length);
+            newStrings[strings.Length] = str;
+            strings = newStrings;
+        }
+        public override void Review()
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                _output = null;
+                return;
+            }
+
+            _output = input.Split(' ');
+            string[] res = new string[0];
+            int counter = 0;
+            for (int i = 0; i < _output.Length;)
+            {
+                string p = "";
+                counter = _output[i].Length;
+                while (counter <= 50)
+                {
+                    p += _output[i++] + " ";
+                    if (i != _output.Length)
+                    {
+                        counter += _output[i].Length + 1;
+                    }
+                    else
+                        break;
+                }
+
+                Add(ref res, p.Substring(0, p.Length - 1));
+            }
+            _output = res;
+        }
+        public override string ToString()
+        {
+            if (_output == null || _output.Length == 0)
+                return string.Empty;
+            return string.Join(Environment.NewLine, _output);
+        }
+    }
+}
