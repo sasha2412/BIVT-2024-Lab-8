a0bf48ab92ca0558f9ea30cf5c1ae7c0e0d4ec2c
Sasha Sivkov <58732809+sasha2412@users.noreply.github.com>
2025-05-27 13:54:59 +0300

    Blue_4.cs

--- Blue_4.cs
+++ Blue_4.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab8
+namespace Lab_8;
+
+public class Blue_4 : Blue
 {
-    internal class Class1
+    private int _output;
+
+    public int Output => _output;
+
+    public Blue_4(string input) : base(input)
     {
+        _output = 0;
+    }
+
+    public override void Review()
+    {
+        if (string.IsNullOrEmpty(input))
+            return;
+        
+        bool wasDigit = false; int num = 0;
+        foreach (char symb in input)
+        {
+            if (Char.IsDigit(symb) && !wasDigit)
+            {
+                num = (int)symb-48;
+                wasDigit = true;
+            }
+            else if (Char.IsDigit(symb) && wasDigit)
+            {
+                num*=10; num += (int)symb-48;
+            }
+            else if (!Char.IsDigit(symb) && wasDigit)
+            {
+                _output+=num;
+                num = 0;
+                wasDigit = false;
+            }
+        }
+    }
+    public override string ToString()
+    {
+        return $"{_output}";
     }
 }
