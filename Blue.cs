a60aeaadfa42501d1e9caedc8899c7a9aeb50955
Sasha Sivkov <58732809+sasha2412@users.noreply.github.com>
2025-06-05 11:50:09 +0300

    Update Blue.cs

--- Blue.cs
+++ Blue.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
-namespace Lab8
+namespace Lab_8
 {
     public abstract class Blue
     {
         private string _input;
-        public string input => _input;
+        public string Input => _input;
+
         public Blue(string input)
         {
             _input = input;
